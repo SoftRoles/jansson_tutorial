f78055b95d68331c5c0f1087b3bd96f686911688
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-02-10 13:38:53 +0300

    add return statement

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <jansson.h>
 
-void main()
+int main()
 {
   size_t i;
   char text[1000] = "[{\"id\": \"a1\"},{\"id\": \"a2\"}]";
@@ -14,11 +14,13 @@ void main()
   if (!root)
   {
     fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+    return 1;
   }
   if (!json_is_array(root))
   {
     fprintf(stderr, "error: string is not an json array\n");
     json_decref(root);
+    return 1;
   }
   for (i = 0; i < json_array_size(root); i++)
   {
@@ -29,16 +31,19 @@ void main()
     {
       fprintf(stderr, "error: json string is not an object\n", i + 1);
       json_decref(root);
+      return 1;
     }
     keyval = json_object_get(object, "id");
     if (!json_is_string(keyval))
     {
       fprintf(stderr, "error: json key value is not a string\n", i + 1);
       json_decref(root);
+      return 1;
     }
     printf("id: %s\n", json_string_value(keyval));
-    json_decref(root);
   }
+  json_decref(root);
 
   printf("Hello World!");
+  return 0;
 }
