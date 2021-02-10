334068592e591fcb96e780106ca50d9b352c1488
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-02-10 13:36:35 +0300

    parse json string

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <jansson.h>
+
+void main()
+{
+  size_t i;
+  char text[1000] = "[{\"id\": \"a1\"},{\"id\": \"a2\"}]";
+
+  json_t *root;
+  json_error_t error;
+
+  root = json_loads(text, 0, &error);
+  if (!root)
+  {
+    fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+  }
+  if (!json_is_array(root))
+  {
+    fprintf(stderr, "error: string is not an json array\n");
+    json_decref(root);
+  }
+  for (i = 0; i < json_array_size(root); i++)
+  {
+    json_t *object, *keyval;
+
+    object = json_array_get(root, i);
+    if (!json_is_object(object))
+    {
+      fprintf(stderr, "error: json string is not an object\n", i + 1);
+      json_decref(root);
+    }
+    keyval = json_object_get(object, "id");
+    if (!json_is_string(keyval))
+    {
+      fprintf(stderr, "error: json key value is not a string\n", i + 1);
+      json_decref(root);
+    }
+    printf("id: %s\n", json_string_value(keyval));
+    json_decref(root);
+  }
+
+  printf("Hello World!");
+}
